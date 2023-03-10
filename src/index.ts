9047256675b335bb96af533e9b52f9953d3882fe
Caian Ertl <caian.ertl@mzgroup.com>
2023-03-10 05:18:53 -0300

    feat: simplify the logic a bit and improve the text

--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,16 @@ export default {
   async fetch (request: Request): Promise<Response> {
     const url = new URL(request.url)
 
-    if (url.pathname !== '/') {
-      return new Response(null, { status: 404 })
-    }
+    const requirements = url.pathname === '/' && request.method === 'GET' && ALLOWED_HOSTNAMES.includes(url.hostname)
 
-    if (request.method !== 'GET') {
-      return new Response(null, { status: 404 })
-    }
-
-    if (!ALLOWED_HOSTNAMES.includes(url.hostname)) {
+    if (!requirements) {
       return new Response(null, { status: 404 })
     }
 
     const [subdomain] = url.hostname.split('.')
 
-    const title = `Welcome to ${url.hostname}!`
-    const heading = `what a ${subdomain}eful site`.toUpperCase()
+    const title = `welcome to my ${url.hostname}!`.toUpperCase()
+    const heading = `thank you for visiting this ${subdomain}eful site`.toUpperCase()
     const html = Mustache.render(content, { title, heading })
 
     const headers = {
